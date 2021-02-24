86e1880ed20de356b4c7485fc1aef08173bd06c3
kelvin <kelvin@jenga-agency.com>
2021-02-24 20:21:10 +0300

    solve digital root

---
+++ codewars/digital-root.exs
@@ -0,0 +1,18 @@
+defmodule FindDigitalRoot do
+  def digital_root(n) when n < 10 do
+    n
+  end
+
+  def digital_root(n) do
+    digits = Integer.digits(n)
+    sum = Enum.sum(digits)
+    digital_root(sum)
+  end
+end
+
+IO.inspect(FindDigitalRoot.digital_root(456))
+
+# defmodule Kata do
+#    def digital_root(n) when n < 10, do: n
+#    def digital_root(n), do: Integer.digits(n) |> Enum.sum |> digital_root
+#end
