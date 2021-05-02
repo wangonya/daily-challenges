f07bfb27ea0a9d4b9584893983c946886d3c1023
kelvin <kelvin@jenga-agency.com>
2021-05-02 07:21:03 +0300

    add explanation

--- codewars/range_extraction.py
+++ codewars/range_extraction.py
@@ -43,3 +43,14 @@ if __name__ == "__main__":
 
     assert range_extraction(a) == '-6,-3-1,3-5,7-11,14,15,17-20'
     assert range_extraction(b) == '-3--1,2,10,15,16,18-20'
+
+# enumerate takes an iterable and returns a sequence of pairs where the
+# first element is the index in the iterable, and the second is the value
+
+# e.g. enumerate([1, 4, 5, 6, 9]) returns ((0,1), (1,4), (2,5), (3,6), (4,9)).
+
+# If a set of numbers belong to a sequence, their value minus their index should all be the same (assuming the numbers are sorted). So using the previous example:
+# [i for i in map(lambda p: p[1]-p[0], enumerate([1,4,5,6,9]))] returns [1, 3, 3, 3, 5].
+
+# groupby combines all entries with the same key (in this case, the result of the lambda expression):
+# [[j for j in i[1]] for i in groupby(enumerate([1,4,5,6,9]), lambda p: p[1]-p[0])] returns [[(0, 1)], [(1, 4), (2, 5), (3, 6)], [(4, 9)]]
