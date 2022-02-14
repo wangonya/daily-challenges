837a21760d3945d0b81d7265d27c317c1fab7049
wangonya <kwangonya@gmail.com>
2022-02-14 05:53:36 +0300

    solve

--- leetcode/max_subarray.py
+++ leetcode/max_subarray.py
@@ -1,7 +1,9 @@
 from typing import List
+
+
 class Solution:
     # Kadane's algorithm
-    # 
+    #
     # Initializing max_till_now = 0
     # Initializing max_ending = 0
     # Repeat steps 4 to 6 for every element in the array
@@ -11,16 +13,16 @@ class Solution:
     # return max_till_now
 
     def maxSubArray(self, nums: List[int]) -> int:
-        current_highest, overall_highest = float("-inf"), float("-inf") 
+        current_highest, overall_highest = float("-inf"), float("-inf")
         for num in nums:
-            current_highest = max(current_highest+num, num)
+            current_highest = max(current_highest + num, num)
             overall_highest = max(overall_highest, current_highest)
 
         return overall_highest
 
 
 s = Solution()
-assert s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6
+assert s.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
 assert s.maxSubArray([1]) == 1
 assert s.maxSubArray([-1]) == -1
-assert s.maxSubArray([5,4,-1,7,8]) == 23
+assert s.maxSubArray([5, 4, -1, 7, 8]) == 23
