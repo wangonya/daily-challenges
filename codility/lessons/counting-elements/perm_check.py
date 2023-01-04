2ac5ebff9897de13b29f7948f509774e1cc226ae
wangonya <kwangonya@gmail.com>
2023-01-04 11:04:03 +0300

    solve

--- codility/lessons/counting-elements/perm_check.py
+++ codility/lessons/counting-elements/perm_check.py
@@ -1,15 +1,20 @@
+# A permutation is a sequence containing each element from 1 to N once, and only once.
 def solution(A):
     A.sort()
+    if A[0] != 1:
+        return 0
     for i in range(len(A)-1):
-        if A[i] - A[i+1] in (-1,0):
+        if A[i] - A[i+1] == -1:
             continue
         return 0
     return 1
 
+assert solution([2]) == 0
 assert solution([4, 1, 3, 2]) == 1
 assert solution([4, 1, 3]) == 0
 
 
+# almost there - 83%. sum ok but not perm
 # def solution(A):
 #     # sum of first n natural numbers = n(n+1) / 2
 #     n = max(A)
