6a5121b407fc0df3d19f26a45d438649219007a0
wangonya <kwangonya@gmail.com>
2023-01-04 05:54:03 +0300

    attempt - 25% score

--- codility/lessons/counting-elements/perm_check.py
+++ codility/lessons/counting-elements/perm_check.py
@@ -1,9 +1,18 @@
 def solution(A):
-    # sum of first n natural numbers = n(n+1) / 2
-    n = max(A)
-    if sum(A) == (n * (n+1)) / 2:
-        return 1
-    return 0
+    A.sort()
+    for i in range(len(A)-1):
+        if A[i] - A[i+1] in (-1,0):
+            continue
+        return 0
+    return 1
 
 assert solution([4, 1, 3, 2]) == 1
 assert solution([4, 1, 3]) == 0
+
+
+# def solution(A):
+#     # sum of first n natural numbers = n(n+1) / 2
+#     n = max(A)
+#     if sum(A) == (n * (n+1)) / 2:
+#         return 1
+#     return 0
