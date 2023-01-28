96d3617e31e11c500bebc988d244d7c423313480
wangonya <kwangonya@gmail.com>
2023-01-28 06:21:23 +0300

    solve

--- codility/lessons/prefix-sums/count_div.py
+++ codility/lessons/prefix-sums/count_div.py
@@ -1,12 +1,12 @@
-def solution(A, B, K):
-    counter = 0
+import math
 
-    for n in range(A, B + 1):
-        if n % K == 0:
-            counter += 1
-    return counter
+
+def solution(A, B, K):
+    start = math.ceil(A / K)
+    end = math.floor(B / K)
+    return end - start + 1
 
 
 assert solution(6, 11, 2) == 3
 assert solution(11, 345, 17) == 20
-assert solution(0, 2_000_000_000, 1) == 2000000000
+assert solution(0, 2_000_000_000, 1) == 2000000001
